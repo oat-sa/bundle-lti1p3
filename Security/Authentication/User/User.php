066d2c332ea45c76cf697ec326675da74c619366
Makar Sichevoi <makar.sichevoy@taotesting.com>
2024-08-30 18:08:34 +0300

    feat: Upgrade Symfony version

--- Security/Authentication/User/User.php
+++ Security/Authentication/User/User.php
@@ -37,7 +37,7 @@ class User implements UserInterface
 
     public function getUserIdentifier(): string
     {
-        return (string) ($this->userIdentifier ?? 'identifier');
+        return $this->userIdentifier ?? 'identifier';
     }
 
     public function eraseCredentials(): void
